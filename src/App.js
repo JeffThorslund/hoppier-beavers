7669c00e47cc90954be0601c5346d73b00b0924f
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-07-06 23:19:45 -0400

    adding github corner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Logic from "./Logic";
 import "./App.css";
+import GithubCorner from 'react-github-corner';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Logic />
+      <GithubCorner href="https://github.com/JeffThorslund/hoppier-beavers" octoColor="#efe6dd"/>
     </div>
   );
 };
